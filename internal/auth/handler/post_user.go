1fca8e539341f1f88fa77ca25fccf84ebb5ada05
Tghoz <govannytgoz@gmail.com>
2025-05-13 15:31:50 -0400

    Add password hashing in user creation handler for security

--- internal/auth/handler/post_user.go
+++ internal/auth/handler/post_user.go
@@ -4,6 +4,8 @@ import (
 	"system/internal/auth/model"
 	"system/internal/auth/repo"
 
+	"golang.org/x/crypto/bcrypt"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -14,7 +16,15 @@ func CreateUserHandler(authRepo repo.Auth_Repo) gin.HandlerFunc {
 			c.JSON(400, gin.H{"error": "Datos inválidos"})
 			return
 		}
-		// Usar el repositorio para guardar en la DB
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+
+		if err != nil {
+			c.JSON(500, gin.H{"error": "Error al procesar la contraseña"})
+			return
+		}
+		user.Password = string(hashedPassword)
+
 		if err := authRepo.CreateUser(c.Request.Context(), &user); err != nil {
 			c.JSON(500, gin.H{"error": "Error al crear usuario"})
 			return
