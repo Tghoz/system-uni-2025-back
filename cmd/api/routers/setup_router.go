c3e984ad4e229c6fc8705bef333d2c9c3a8229fc
Tghoz <govannytgoz@gmail.com>
2025-05-14 19:11:06 -0400

    Add CORS middleware to the router for improved cross-origin requests handling

--- cmd/api/routers/setup_router.go
+++ cmd/api/routers/setup_router.go
@@ -5,11 +5,14 @@ import (
 	"system/internal/auth/routers"
 	potect "system/internal/auth/routers/protected_routes"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter(userRepo repo.Auth_Repo) {
 	r := gin.Default()
+
+	r.Use(cors.Default())
 
 	routers.UserRouter(r, userRepo)
 	potect.AdminRouter(r, userRepo)
